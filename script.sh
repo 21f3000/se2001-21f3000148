6f23f50fa57415b5470430589d82556c7fbe7b7b
21f3000 <137524781+21f3000@users.noreply.github.com>
2024-06-17 20:46:35 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Shell variables
+export GITHUB_USERNAME="21f3000"
+export GITHUB_REPOSITORY="se2001-21f3000148"
+
+echo "Variables exported:"
+echo "GITHUB_USERNAME: $GITHUB_USERNAME"
+echo "GITHUB_REPOSITORY: $GITHUB_REPOSITORY"
